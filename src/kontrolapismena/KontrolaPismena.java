47828580eea59a98ee93a8c16548ad551b225abb
Vaclav Pavlicek <vaclav@pavlicek.cz>
2013-12-04 09:47:56 +0100

    Kratší verze.

--- src/kontrolapismena/KontrolaPismena.java
+++ src/kontrolapismena/KontrolaPismena.java
@@ -21,41 +21,24 @@ public class KontrolaPismena {
          char symbol = 'A' ;
          symbol = (char) (128.0 * Math.random());   //Generuje náhodný znak.
          
-         if (symbol >= 'A' ){   //Je to A nebo větší.
-         
-             if (symbol <= 'Z'){    //ano , a je to Znebo menší ?
-                 // Potom to je velké písmeno.
-                 
-                 System.out.println("Dostali jste velké písmeno " + symbol );
-                 
-             }else{ // Není to Z nebo menší.
-             
-                 if (symbol >= 'a' ){   //A je to a nebo větší.
-                 
-                     if (symbol <= 'z' ){   // Ano ,je to menší než z?
-                         // Tak je to malé písmeno.
-                         
-                         System.out.println("Dostali jste malé písmeno " + symbol);
-                         
-                     }else{     //Je to více než a a není to písmeno.
-                     
-                         System.out.println("Kód je větší než a a není to písmeno, je to " + symbol);
-                         
-                     }
-                     }else{     //Není to méně než z.
-                     
-                         System.out.println("Kód je menší než a a není to písmeno , je to " + symbol);
-                         
-                     }
-                
-             }
-             
-         }else{
-         
-             System.out.println("Kód je menší než A, takže to není písmeno.");
-             
-         }
-         
+        if (symbol >= 'A' && symbol <= 'Z'){    //Je to velké písmeno.
+        
+            System.out.println("Dostali jste velké písmeno " + symbol);
+            
+        } else {
+        
+          if (symbol >= 'a' && symbol <= 'z' )  {   // Nebo malé ?
+          
+              System.out.println("Dostali jste malé písmeno " + symbol);
+          
+          } else {      //Není to méně než z
+          
+              System.out.println("Kód není žádné písmeno");
+              
+          }
+            
+        }
+        
     }
     
 }
